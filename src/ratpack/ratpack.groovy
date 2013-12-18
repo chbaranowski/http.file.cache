ce76e73625eea00d9180400b761f089d303da7ff
Christian Baranowski <tux2323@pro-pix.de>
2013-12-18 09:21:08 +0100

    add ratpack based cache implementation for pear download.

---
+++ src/ratpack/ratpack.groovy
@@ -0,0 +1,23 @@
+import static ratpack.groovy.Groovy.*
+
+import org.apache.commons.io.FileUtils;
+
+ratpack {
+    handlers {
+        handler {
+            def uri = request.uri
+            File cache = new File("cache")
+            File cachedFile = new File(cache, uri)
+            if(!cachedFile.exists()) {
+                URL pearURL = new URL("http://download.pear.php.net$uri")
+                InputStream is = pearURL.openStream()
+                try {
+                    FileUtils.copyInputStreamToFile(is, cachedFile)
+                } finally {
+                    is.close()
+                }
+            }
+            response.send(new FileInputStream(cachedFile))
+        }    
+    }
+}
